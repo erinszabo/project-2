bb2e6ee7593f5dbd18674f9354811db37430448e
Erin Szabo <eszabo@cocc.edu>
2023-10-13 15:01:22 -0700

    Parse the same way project 1 did.

--- web/app.py
+++ web/app.py
@@ -2,18 +2,24 @@
 John Doe's Flask API.
 """
 
-from flask import Flask
+from flask import Flask, abort, send_from_directory, render_template
 import config
 
 app = Flask(__name__)
+options = config.configuration()
+root = options.DOCROOT
+port = options.PORT
 
 
-
-@app.route("/")
+@app.route("/h")
 def hello():
     return "UOCIS docker demo!\n"
 
+@app.route("/")
+def index():
+    return send_from_directory(root, 'trivia.html')
+# return send_from_directory('pages/', 'trivia.html')
+
 if __name__ == "__main__":
-    options = config.configuration()
-    root = options.DOCROOT
-    app.run(debug=True, host='0.0.0.0')
+
+    app.run(debug=True, host='0.0.0.0', port=port)
