95b1c928737ff3b8c6ecf323870972043d5619dc
Erin Szabo <eszabo@cocc.edu>
2023-10-14 18:28:37 -0700

    Implement file checking

--- web/app.py
+++ web/app.py
@@ -1,25 +1,36 @@
 """
-John Doe's Flask API.
+Erin Szabo's Flask API.
 """
 
-from flask import Flask, abort, send_from_directory, render_template
+from flask import Flask, abort, send_from_directory
 import config
 
 app = Flask(__name__)
-options = config.configuration()
-root = options.DOCROOT
-port = options.PORT
 
 
-@app.route("/h")
+@app.route("/")
 def hello():
     return "UOCIS docker demo!\n"
 
-@app.route("/")
-def index():
-    return send_from_directory(root, 'trivia.html')
-# return send_from_directory('pages/', 'trivia.html')
+# return abort(403) to use this
+@app.errorhandler(403)
+def forbidden(e):
+    return send_from_directory(root, '403.html'), 403
+
+@app.errorhandler(404)
+def notfound(e):
+    return send_from_directory(root, '404.html'), 404
+
+@app.route("/<string:file>")
+def filecheck(file):
+    if ".." in file or "~" in file:
+        return abort(403)
+    return send_from_directory(root, file), 200
+
 
 if __name__ == "__main__":
+    options = config.configuration()
+    root = options.DOCROOT
+    port = options.PORT
 
     app.run(debug=True, host='0.0.0.0', port=port)
